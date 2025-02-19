df664c8d0a03c006bbaa16752e15b7ad758d25b8
xeiavicaflashrepository <197413438+xeiavicaflashrepository@users.noreply.github.com>
2025-02-19 11:25:11 -0500

    Fix script

--- runswpro.bat
+++ runswpro.bat
@@ -2,5 +2,4 @@
 
 .REM This script lets you have the default loaded directory be where you are currently at.
 .REM This assumes it was placed under C:\SWPRO22
-CD C:\SWPRO22
-.\swpro.com
+C:\SWPRO22\SWPRO.COM C:\SWPRO22
